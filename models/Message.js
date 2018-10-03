f00dcf1dc076cb66d8eedf00b1d24ee06d658251
Dennis Onder <webdev.dennisonder@gmail.com>
2018-10-03 03:12:06 +0200

    Added Message Schema

---
+++ models/Message.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const MessageSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'users',
+  },
+  message: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = Message = mongoose.model('messages', MessageSchema);
