6a389ff401458ecf9222203e8abdcc20feaac8a0
Dennis Onder <webdev.dennisonder@gmail.com>
2018-10-03 02:27:15 +0200

    Folder and files renamed for convenience.

--- express.js
+++ express.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 8000;
 const db = require('mongoose');
 const key = require('./config/keys').mongoURI;
 const bodyParser = require('body-parser');
-const registerUser = require('./routes/api/registerUser');
+const users = require('./routes/api/users');
 
 // Body Parser Middleware
 app.use(bodyParser.urlencoded({extended: false}));
@@ -24,6 +24,6 @@ app.get('/test', (req, res) => {
 });
 
 // Routes
-app.use('/api/users', registerUser);
+app.use('/api/users', users);
 
 app.listen(port, () => console.log(`Server running on port: ${port}.`));
